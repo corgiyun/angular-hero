2f11061fa2c12f7fa24d21957b30a2332bd2dbbf
lim <569212066@qq.com>
2021-11-30 13:43:10 +0800

    init angular

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.less'],
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
